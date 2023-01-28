d4bd7fe3f191be042ba7031d2ade3d959da715d5
AlexeyR888 <lex.raikov@gmail.com>
2023-01-28 11:28:23 +0200

    Test 0.1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("main")
+class Car:
+    wheels = 4
+    speed = 120
+    color = 'Green'
+    power = 85
+
+
+print(Car.wheels)
+print(Car.speed)
+Car.speed = 160
+print(Car.speed)
+Car.power = 105
+print(Car.power)
+
+class Table:
+    height = 90
+    width = 120
+
+print(Table.height)
