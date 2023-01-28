21e73cbea7491a088d7c42eb131558f15bb0f9cd
AlexeyR888 <lex.raikov@gmail.com>
2023-01-28 11:46:37 +0200

    Test 0.1

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 class Car:
-    wheels = 4
-    speed = 120
-    color = 'Green'
-    power = 85
+    wheel1 = 4
+    speed1 = 120
+    color1 = 'Green'
+    power1 = 85
+    year1 = 2011
 
-
-print(Car.wheels)
-print(Car.speed)
-Car.speed = 160
-print(Car.speed)
-Car.power = 105
-print(Car.power)
+print(Car.wheel1)
+print(Car.color1)
+print(Car.speed1)
+print(Car.power1)
+print(Car.year1)
 
 class Table:
-    height = 90
-    width = 120
+    height2 = 90
+    width2 = 120
+    weight2 = 30
+    maretial2 = 'wood'
+    color2 = 'brown'
 
-print(Table.height)
+print(Table.height2)
+print(Table.width2)
+print(Table.weight2)
+print(Table.maretial2)
+print(Table.color2)
+
+
+class Tree:
+    height3 = 560
+    width3 = 90
+    weight3 = 180
+    where3 = 'park'
+    age3 = 60
+
+print(Tree.height3)
+print(Tree.width3)
+print(Tree.weight3)
+print(Tree.where3)
+print(Tree.age3)
